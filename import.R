16d386868b980357264fb52aa820cfef21b626cc
Anton Lebedevich <mabrek@gmail.com>
2012-12-09 20:39:58 +0400

    read all files and merge them

--- import.R
+++ import.R
@@ -3,3 +3,9 @@ files <- list.files()
 readfile <- function(name) {
   read.table(name, na.strings="None", colClasses=c("numeric", "numeric"), col.names=c("time", name))
 }
+
+allMetrics <- lapply(files, readfile)
+
+# http://www.r-bloggers.com/merging-multiple-data-files-into-one-data-frame/
+
+mergedMetrics <- Reduce(function(x, y) {merge(x,y)}, allMetrics)
