501e0cc204086a23b4b32c1cd7c4c22c3456295d
Anton Lebedevich <mabrek@gmail.com>
2012-12-09 22:49:26 +0400

    exclude NA

--- import.R
+++ import.R
@@ -1,13 +1,16 @@
 files <- list.files()
 
 readfile <- function(name) {
-  data <- read.table(name, na.strings="None", colClasses=c("integer", "numeric"), col.names=c("time", name))
-  data <- data[!is.na(data[[2]]),]
+  data <- na.exclude(read.table(name, na.strings="None", colClasses=c("integer", "numeric"), col.names=c("time", name)))
+  if(nrow(data) == 0 | (min(data[[2]]) == max(data[[2]]))) {
+     NA
+   } else {
+     data
+   }
 }
 
 # use parallel versions for reading and merging
-allMetrics <- lapply(files, readfile)
+allMetrics <- na.exclude(lapply(files, readfile))
 
 # http://www.r-bloggers.com/merging-multiple-data-files-into-one-data-frame/
-
-mergedMetrics <- Reduce(function(x, y) {merge(x,y, "time")}, allMetrics)
+mergedMetrics <- Reduce(function(x, y) {merge(x,y)}, allMetrics)
