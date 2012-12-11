4080b52ec480907e22b9087087df992636ea83e9
Anton Lebedevich <mabrek@gmail.com>
2012-12-11 12:09:54 +0400

    todo, correlation matrix

--- import.R
+++ import.R
@@ -5,14 +5,18 @@ readfile <- function(name) {
   if(nrow(data) == 0) {
      NA
    } else {
+     # TODO exclude noise-like and mostly constant
      if (min(data[[2]]) == max(data[[2]])) NA else data
    }
 }
 
-# use parallel versions for reading and merging
+# TODO use parallel versions for reading and merging
 allMetrics <- lapply(files, readfile)
 
 existingMetrics <- allMetrics[!is.na(allMetrics)]
 
 # http://www.r-bloggers.com/merging-multiple-data-files-into-one-data-frame/
 mergedMetrics <- Reduce(function(x, y) {merge(x,y, by="time", all=TRUE)}, existingMetrics)
+
+correlated <- cor(mergedMetrics, use="pairwise.complete.obs")
+correlated[is.na(correlated)] <- 0
