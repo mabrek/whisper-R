eebf787fda446056751dc539c8ab3a61aba3e7e2
Anton Lebedevich <mabrek@gmail.com>
2012-12-17 10:21:24 +0400

    remove arimaScore (slow and doesn't show anomalies in a slowly changing values)

--- analyse.R
+++ analyse.R
@@ -9,20 +9,6 @@ distance <- function(correlated) {
   as.dist(1-abs(correlated))
 }
 
-# the code below is based on ChangeAnomalyDetection package
-arimaScore <- function (x, term = 30, ...) 
-{
-  sapply(1:(length(x) - term - 1), function(i) {
-    train <- x[i:(i + term)]
-    target <- x[(i + term + 1)]
-    fit <- auto.arima(train, ...)
-    pred <- forecast(fit, h = 1)$mean[1]
-    m <- mean(fit$residuals)
-    s <- sd(fit$residuals)
-    -log(dnorm(pred - target, m, s))
-  })
-}
-
 linearScore <- function (x, term = 30, ...) {
   score <- sapply(1:(length(x) - term), function(i) {
     train <- x[i:(i + term - 1)]
