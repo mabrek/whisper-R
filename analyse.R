8ebed3bfae16d23ab82f8311973dc6aca77e6ff7
Anton Lebedevich <mabrek@gmail.com>
2012-12-18 10:40:34 +0400

    allow 2 variables in linearScoreVector

--- analyse.R
+++ analyse.R
@@ -9,11 +9,15 @@ distance <- function(correlated) {
   as.dist(1-abs(correlated))
 }
 
-linearScoreVector <- function (x, term = 30, ...) {
+linearScoreVector <- function (x, y, term = 30, ...) {
+  missingY = missing(y)
   score <- sapply(1:(length(x) - term), function(i) {
-    train <- x[i:(i + term - 1)]
-    fit <- lm(b ~ a, data.frame(a=1:term, b=train))
-    1 - summary(fit)$adj.r.squared
+    if(missingY) {
+      df = data.frame(a = 1:term, b = x[i:(i + term - 1)])
+    } else {
+      df = data.frame(a = x[i:(i + term - 1)], b = y[i:(i + term - 1)])
+    }
+    1 - summary(lm(b ~ a, df, ...))$adj.r.squared
   })
   c(rep(0, round(term/2)), score, rep(0, term - round(term/2)))
 }   
