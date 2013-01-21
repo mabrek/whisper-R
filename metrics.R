7bec3881bec50cdebf55edf35cab015f07bc2e3c
Anton Lebedevich <mabrek@gmail.com>
2013-01-21 12:29:09 +0400

    use facet labels instead of colour

--- metrics.R
+++ metrics.R
@@ -103,6 +103,6 @@ find.changed.sd <- function(metrics, a, b, n=25) {
 }
 
 multiplot <- function(metrics) {
-  ggplot(aes(x = Index, y = Value, group = Series, colour = Series),
-         data = fortify(metrics, melt = TRUE)) + geom_line() + xlab("") + ylab("") + facet_grid(Series ~ ., scales = "free_y") + opts(strip.text.y = theme_text())
+  ggplot(aes(x = Index, y = Value),
+         data = fortify(metrics, melt = TRUE)) + geom_line() + xlab("") + ylab("") + facet_grid(Series ~ ., scales = "free_y") + theme(strip.text.y = element_text(angle=0))
 }
