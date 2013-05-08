c0b716b1124e4ee93cc4545b3fe93719ee76181b
Anton Lebedevich <mabrek@gmail.com>
2013-05-08 11:38:16 +0400

    multiplot: filter out only all-na columns

--- metrics.R
+++ metrics.R
@@ -155,7 +155,7 @@ filter.colnames <- function(pattern, metrics, invert=FALSE) {
 
 multiplot <- function(metrics) {
   data <- metrics[,
-                  which(sapply(metrics[], function(v) {all(!is.na(v))})),
+                  which(sapply(metrics, function(v) {!all(is.na(v))})),
                   drop=FALSE]
   ggplot(aes(x = Index, y = Value),
          data = fortify(data, melt = TRUE)) + geom_line() + xlab("") + ylab("") + facet_grid(Series ~ ., scales = "free_y") + theme(strip.text.y = element_text(angle=0), axis.text.y = element_blank(), axis.ticks.y = element_blank())
