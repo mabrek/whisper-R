8d528518cf7f7e9ec7d0dd390cfb95b6923a97a8
Anton Lebedevich <mabrek@gmail.com>
2013-05-12 13:13:17 +0400

    mean is a little bit faster

--- metrics.R
+++ metrics.R
@@ -189,7 +189,7 @@ multiplot <- function(metrics, limit=50) {
   k <- min(n, limit)
   repeat {
     m <- data[, i:k, drop=FALSE]
-    ms <- rollapply(m, width=max(10, trunc(r/100)), FUN=median, na.rm=TRUE, fill=NA)
+    ms <- rollapply(m, width=max(10, trunc(r/100)), FUN=mean, na.rm=TRUE, fill=NA)
     df <- data.frame(index(m)[rep.int(1:r, k)],
                      factor(rep(1:k, each = r), levels = 1:k, labels = colnames(m)),
                      as.vector(coredata(m)),
