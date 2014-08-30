fbf734ac59b82f090c5fe963421a07eade8f2208
Anton Lebedevich <mabrek@gmail.com>
2014-08-30 22:32:04 +0400

    filter codahale like metrics

--- metrics.R
+++ metrics.R
@@ -112,8 +112,8 @@ filter.statsd <- function(metrics) {
                   TRUE)
 }
 
-filter.folsom <- function(metrics) {
-  metrics <- filter.colnames("\\.acceleration\\.[^\\.]+$|\\.(day|fifteen|five|mean|one)$|\\.(arithmetic_mean|geometric_mean|harmonic_mean|kurtosis|median|skewness|standard_deviation|variance)$|\\.reductions_since_last_call$|\\.n$",
+filter.codahale_like <- function(metrics) {
+  metrics <- filter.colnames("\\.acceleration\\.[^\\.]+$|\\.(day|fifteen|five|mean|one)$|\\.(arithmetic_mean|geometric_mean|harmonic_mean|kurtosis|skewness|standard_deviation|variance)$|\\.reductions_since_last_call$|\\.n$|\\.stddev$|MinuteRate$",
                              metrics,
                              TRUE)
   columns <- colnames(metrics)
