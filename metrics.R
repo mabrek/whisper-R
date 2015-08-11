51145fe44813344bccfac76e9c6468f4521d108b
Anton Lebedevich <mabrek@gmail.com>
2015-08-11 22:58:33 +0300

    dplyr version of jmeter request concurency (fails on big data)

--- metrics.R
+++ metrics.R
@@ -1,4 +1,3 @@
-library(dplyr)
 library(parallel)
 library(zoo)
 library(ggplot2)
@@ -16,6 +15,8 @@ library(tsne)
 library(shiny)
 library(dygraphs)
 library(stringr)
+library(bit64)
+library(dplyr)
 
 lsd <- function(pos=1) {
   names(grep("^function$",
@@ -67,12 +68,13 @@ read.jmeter.csv <- function(file.name) {
         colClasses=c("character", "numeric", "factor", "factor", "NULL", "NULL",
             "logical", "numeric", "integer", "integer", "numeric", "numeric", 
             "numeric"))
+    m[,"timeStamp.raw"] <- m[,"timeStamp"]
     m[,"timeStamp"] <- as.POSIXct(substr(m[,"timeStamp"], 1, 10), origin="1970-01-01 00:00:00", format='%s')
     m
 }
 
 aggregate.jmeter <- function(jmeter, interval.seconds) {
-    columns <- setdiff(colnames(jmeter), "label")
+    columns <- setdiff(colnames(jmeter), c("label", "responseCode", "timeStamp.raw"))
     tree.merge.xts(
         mclapply(
             c(levels(jmeter[,"label"]), ""),
@@ -94,8 +96,20 @@ aggregate.jmeter <- function(jmeter, interval.seconds) {
                 elapsed.median <- as.xts(aggregate(elapsed.raw, ticks, median, na.rm = TRUE))
                 elapsed.percentile99 <- as.xts(aggregate(elapsed.raw, ticks, quantile, probs = c(0.99), na.rm = TRUE))
                 threads.median <- as.xts(aggregate(z[,"grpThreads"], ticks, median, na.rm = TRUE))
-                aggregated <- merge.xts(success, error, elapsed.min, elapsed.max, elapsed.median, elapsed.percentile99, threads.median)
-                colnames(aggregated) <- str_c(prefix, ".", c("success.rate", "error.rate", "elapsed.min", "elapsed.max", "elapsed.median", "elapsed.percentile99", "threads.median"))
+                start.time = as.integer64(jmeter[idx,"timeStamp.raw"]) + jmeter[idx,"Connect"]
+                end.time = as.integer64(jmeter[idx,"timeStamp.raw"]) + jmeter[idx,"elapsed"]
+                concurrency.raw <-
+                    rbind(data.frame(timestamp=start.time, value=1), 
+                          data.frame(timestamp=end.time, value=-1)) %>% 
+                    arrange(timestamp, desc(value)) %>% 
+                    mutate(timestamp=as.POSIXct(timestamp / 1000,
+                                                origin="1970-01-01 00:00:00"), 
+                           concurrency=cumsum(value)) %>% 
+                    select(timestamp, concurrency) %>%
+                        read.zoo
+                concurrency <- as.xts(aggregate(concurrency.raw, align.time(index(concurrency.raw), interval.seconds), max, na.rm = TRUE))
+                aggregated <- merge.xts(success, error, elapsed.min, elapsed.max, elapsed.median, elapsed.percentile99, threads.median, concurrency)
+                colnames(aggregated) <- str_c(prefix, ".", c("success.rate", "error.rate", "elapsed.min", "elapsed.max", "elapsed.median", "elapsed.percentile99", "threads.median", "concurrency"))
                 aggregated
             }))
 }
